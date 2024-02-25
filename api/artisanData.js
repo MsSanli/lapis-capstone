98cc64a9f376620bae9373169578ef393ac45301
Maria Sanli <marisanli.93@gmail.com>
2024-02-25 18:52:33 -0500

    started on read and create api calls and components

--- api/artisanData.js
+++ api/artisanData.js
@@ -1,15 +1,38 @@
+import { clientCredentials } from '../utils/client';
+
 // ARTISAN API CALLS
 // VIEW/READ ALL ARTISANS
-const endpoint = (uid) => new Promise ((resolve, reject) => {
+const endpoint = clientCredentials.databaseURL;
+
+const getArtisans = () => new Promise((resolve, reject) => {
   fetch(`${endpoint}/artisans.json?orderBy="uid"&equalTo="{uid}"`, {
-    method: 'GET', 
+    method: 'GET',
     headers: {
-      'Content-Type': 'applicaton/json', 
+      'Content-Type': 'applicaton/json',
     },
   })
-  .then((response) => response.json())
-  .then((data) => resolve(Object.values(data)))
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
 });
 // CREATE ARTISANS
 // UPDATE ARTISANS
-// DELETE ARTISANS 
+// DELETE ARTISANS
+// FIXME: DELETE AUTHOR
+const deleteSingleArtisan = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/artisans/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getArtisans,
+  deleteSingleArtisan,
+};
+// remember this is singular and when you have more promises, you'll have to put them in brackets and indent.
