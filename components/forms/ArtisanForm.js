5df03285968c5cc9cb8c4f660d9944ffc51aafdc
Maria Sanli <marisanli.93@gmail.com>
2024-03-08 17:25:14 -0500

    fixed dropdown again

--- components/forms/ArtisanForm.js
+++ components/forms/ArtisanForm.js
@@ -11,6 +11,7 @@ const initialState = {
   image: '',
   location: '',
   email: '',
+  wishlist: false,
   // need form element
 };
 
@@ -80,10 +81,9 @@ function ArtisanForm({ obj }) {
       </FloatingLabel>
 
       {/* LOCATION INPUT  */}
-
-      {/* LOCATION DROPDOWN */}
-      <FloatingLabel controlId="floatingSelect" label="Select Location" className="mb-3">
-        <Form.Select
+      <FloatingLabel controlId="floatingInput3" label="Select Borough" className="mb-3">
+        <Form.Control
+          as="select"
           name="location"
           value={formInput.location}
           onChange={handleChange}
@@ -91,11 +91,12 @@ function ArtisanForm({ obj }) {
         >
           <option value="">Select Location</option>
           <option value="Brooklyn">Brooklyn</option>
-          <option value="Queens">Queens</option>
           <option value="Bronx">Bronx</option>
           <option value="Manhattan">Manhattan</option>
           <option value="Staten Island">Staten Island</option>
-        </Form.Select>
+          <option value="Queens">Queens</option>
+          {/* Add more options as needed */}
+        </Form.Control>
       </FloatingLabel>
 
       {/* EMAIL INPUT  */}
