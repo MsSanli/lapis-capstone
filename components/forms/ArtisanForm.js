915b2684fd8afaa6e2cf2b560feb8b0f9b7a12cb
Maria Sanli <marisanli.93@gmail.com>
2024-03-09 11:41:12 -0500

    fixing drop again

--- components/forms/ArtisanForm.js
+++ components/forms/ArtisanForm.js
@@ -81,15 +81,22 @@ function ArtisanForm({ obj }) {
       </FloatingLabel>
 
       {/* LOCATION INPUT  */}
-      <FloatingLabel controlId="floatingInput3" label="Enter Location!" className="mb-3">
+      <FloatingLabel controlId="floatingInput3" label="Select Borough" className="mb-3">
         <Form.Control
-          type="location"
-          placeholder="Enter locatoin"
+          as="select"
           name="location"
           value={formInput.location}
           onChange={handleChange}
           required
-        />
+        >
+          <option value="">Select Location</option>
+          <option value="Brooklyn">Brooklyn</option>
+          <option value="Bronx">Bronx</option>
+          <option value="Manhattan">Manhattan</option>
+          <option value="Staten Island">Staten Island</option>
+          <option value="Queens">Queens</option>
+          {/* Add more options as needed */}
+        </Form.Control>
       </FloatingLabel>
 
       {/* EMAIL INPUT  */}
