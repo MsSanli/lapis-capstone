862ba6945de6c231b98b13e170ea74f3370d9baa
Maria Sanli <marisanli.93@gmail.com>
2024-03-21 20:20:57 -0400

    description showing

--- pages/artisan/[firebaseKey].js
+++ pages/artisan/[firebaseKey].js
@@ -1,3 +1,45 @@
+// /* eslint-disable @next/next/no-img-element */
+// import React, { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { viewArtisanDetails } from '../../api/mergeData';
+// import ReviewCard from '../../components/ReviewCard';
+
+// export default function ArtisanDetailsPage() {
+//   const [artisanDetails, setArtisanDetails] = useState({});
+//   const router = useRouter();
+//   const { firebaseKey } = router.query;
+
+//   useEffect(() => {
+//     viewArtisanDetails(firebaseKey).then(setArtisanDetails);
+//   }, [firebaseKey]);
+
+//   return (
+//     <>
+//       <div className="mt-5 d-flex flex-wrap">
+//         <div className="d-flex flex-column">
+//           <img src={artisanDetails.image} alt={artisanDetails.name} style={{ width: '200px' }} />
+//         </div>
+//         <div className="text-white ms-5 details">
+//           <h5>
+//             {artisanDetails.name} {artisanDetails.wishlist ? '🤍' : ''}
+//           </h5>
+//           Artisan Email: <a href={`mailto:${artisanDetails.email}`}>{artisanDetails.email}</a>
+//           <hr />
+//           <p className="flex-column">{artisanDetails.description || ''}</p>
+//         </div>
+//       </div>
+//       <hr />
+//       <div className="d-flex flex-wrap">
+//         {artisanDetails.reviews?.map((review) => (
+//           <ReviewCard key={review.firebaseKey} reviewObj={review} onUpdate={viewArtisanDetails} />
+//         ))}
+//       </div>
+//     </>
+//   );
+// }
+
+// export { ArtisanDetailsPage };
+
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
@@ -15,16 +57,17 @@ export default function ArtisanDetailsPage() {
 
   return (
     <>
-      <div className="mt-5 d-flex flex-wrap">
-        <div className="d-flex flex-column">
+      <div className="mt-5 d-flex align-items-start">
+        <div className="me-5">
           <img src={artisanDetails.image} alt={artisanDetails.name} style={{ width: '200px' }} />
         </div>
-        <div className="text-white ms-5 details">
+        <div className="text-white details">
           <h5>
-            {artisanDetails.name}
+            {artisanDetails.name} {artisanDetails.wishlist ? '🤍' : ''}
           </h5>
-          Artisan Email: <a href={`mailto:${artisanDetails.email}`}>{artisanDetails.email}</a>
           <hr />
+          <p>{artisanDetails.description || ''}</p>
+          <p>Artisan Email: <a href={`mailto:${artisanDetails.email}`}>{artisanDetails.email}</a></p>
         </div>
       </div>
       <hr />
